088fa505f0d71dfa0e6d67eccd5e7e22d6af838c
hoerzz <rudinhoer160@gmail.com>
2025-07-16 19:00:45 +0700

    update form penerbit

--- app/components/client/Client.tsx
+++ app/components/client/Client.tsx
@@ -44,7 +44,7 @@ export default function ClientLayout({
             <div className="flex flex-1">
               <div className="flex-grow">{children}</div>
             </div>
-            {pathname === "/form-pemodal" ? "" : <Footer />}
+            {pathname === "/form-pemodal" || pathname === "/form-penerbit" ? "" : <Footer />}
           </main>
         )}
         <ModalLogout />
