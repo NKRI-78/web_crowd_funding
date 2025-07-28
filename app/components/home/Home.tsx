e583c256b037a3fcd8737b44694f5bb11badb4bc
Rangga adi putra <ranggadiputra23@gmail.com>
2025-07-28 17:23:55 +0700

    update color bg wa

--- app/components/home/Home.tsx
+++ app/components/home/Home.tsx
@@ -115,15 +115,11 @@ const Home: React.FC = () => {
           <div className="space-y-6 text-center md:text-right items-center md:items-end flex flex-col">
             <div>
               <p className="text-md text-white">Dana Tersalurkan</p>
-              <p className="text-2xl font-bold text-[#4CD137]">
-                Rp 0
-              </p>
+              <p className="text-2xl font-bold text-[#4CD137]">Rp 0</p>
             </div>
             <div>
               <p className="text-md text-white">Pengembalian Dana</p>
-              <p className="text-2xl font-bold text-[#4CD137]">
-                Rp 0
-              </p>
+              <p className="text-2xl font-bold text-[#4CD137]">Rp 0</p>
             </div>
             <div>
               <p className="text-md text-white">Rata-rata Realisasi ROI</p>
@@ -498,7 +494,7 @@ const Home: React.FC = () => {
       </section>
 
       <div className="fixed bottom-4 right-4 z-50">
-        <button className="flex items-center bg-[#4CD137] text-white px-4 py-2 font-bold rounded-full shadow-lg">
+        <button className="flex items-center bg-[#29a71a] text-white px-4 py-2 font-bold rounded-full shadow-lg">
           <img src="/images/wa.png" alt="WA" className="w-5 h-5 mr-2" />
           Butuh Bantuan?
         </button>
