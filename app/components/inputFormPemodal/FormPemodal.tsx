45b2f29ef21c9373db6f816f1fd382fa2baa3a16
Rangga adi putra <ranggadiputra23@gmail.com>
2025-08-04 14:30:50 +0700

    update loading

--- app/components/inputFormPemodal/FormPemodal.tsx
+++ app/components/inputFormPemodal/FormPemodal.tsx
@@ -74,6 +74,7 @@ const FormPemodal: React.FC = () => {
   const isUpdate = searchParams.get("update") === "true";
   const form = searchParams.get("form");
   const [dataProfile, setDataProfile] = useState<DataProfile | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     // const userCookie = Cookies.get("user");
@@ -101,20 +102,18 @@ const FormPemodal: React.FC = () => {
       // }
 
       try {
-        const response = await axios.get(
-          "https://api-capbridge.langitdigital78.com/api/v1/profile",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
+        setIsLoading(true);
+        const response = await axios.get(`${API_BACKEND}/api/v1/profile`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
 
         const data = response.data?.data;
 
         setDataProfile({ ...data, form: form });
 
-        if (data) {
+        if (data && isUpdate) {
           localStorage.setItem("dataProfile", JSON.stringify(data));
           const placeDateBirth = `${data.investor.ktp?.place_datebirth}, ${data.investor.ktp?.place_datebirth}`;
           const [placeOfBirth, dateOfBirth] = placeDateBirth.split(", ");
@@ -125,7 +124,12 @@ const FormPemodal: React.FC = () => {
             nik: data.investor.ktp.nik || "",
             tempatLahir: placeOfBirth || "",
             tanggalLahir: dateOfBirth || "",
-            jenisKelamin: data.gender === "L" ? "Laki-Laki" : "Perempuan",
+            jenisKelamin:
+              data.gender === "L"
+                ? "Laki-Laki"
+                : data.gender === "P"
+                ? "Perempuan"
+                : "",
             statusPernikahan: data.status_marital || "",
             pendidikanTerakhir: data.last_education || "",
             pekerjaan: data.occupation || "",
@@ -191,6 +195,8 @@ const FormPemodal: React.FC = () => {
           "Gagal mengambil profil:",
           error.response?.data || error.message
         );
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -946,6 +952,36 @@ const FormPemodal: React.FC = () => {
       }
     }
   };
+
+  if (isLoading) {
+    return (
+      // <div className="flex items-center justify-center h-[60vh]">
+      //   <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500 border-solid"></div>
+      // </div>
+
+      <div className="flex items-center justify-center h-[60vh] md:mt-52">
+        <div role="status">
+          <svg
+            aria-hidden="true"
+            className="w-12 h-12 text-gray-200 animate-spin dark:text-gray-400 fill-[#4821c1]"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="bg-white w-full mx-auto text-black px-10 md:px-24 py-20">
