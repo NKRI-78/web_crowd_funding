cfb991a3dfb38d6cd0719ecd51aab799af74f149
Rangga adi putra <ranggadiputra23@gmail.com>
2025-08-12 17:24:54 +0700

    type react step progress bar

---
+++ app/src/react-step.tsx
@@ -0,0 +1,23 @@
+declare module "react-step-progress-bar" {
+  import * as React from "react";
+
+  export interface ProgressBarProps {
+    percent: number;
+    filledBackground?: string;
+    unfilledBackground?: string;
+    height?: string | number;
+    stepPositions?: number[];
+    children?: React.ReactNode;
+  }
+
+  export interface StepProps {
+    transition?: string;
+    children?: (props: {
+      accomplished: boolean;
+      index: number;
+    }) => React.ReactNode;
+  }
+
+  export const ProgressBar: React.FC<ProgressBarProps>;
+  export const Step: React.FC<StepProps>;
+}
